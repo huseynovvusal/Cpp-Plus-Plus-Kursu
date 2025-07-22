52e478a522a55313d29f034bfbcdbb136fc6eca5
Vusal Huseynov <vslh333222111@gmail.com>
2025-07-22 18:11:47 +0400

    Add main.cpp with basic C++ program demonstrating output and arithmetic operations

---
+++ Output/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << "\n";
+    cout << "C++\n\nPython\n\nJava\n\nPHP";
+
+    cout << endl
+         << endl;
+
+    cout << 30 - 20 << endl;
+    cout << 5 * 6 << endl;
+
+    cout << "****" << endl;
+    cout << "*  *" << endl;
+    cout << "*  *" << endl;
+    cout << "****" << endl;
+
+    cout << endl
+         << endl;
+
+    cout << "****\n*  *\n*  *\n****" << endl;
+
+    return 0;
+}
