e81924fc180d99d73cac53f7782b7d1117592581
Vusal Huseynov <vslh333222111@gmail.com>
2025-07-22 15:19:35 +0400

    Add initial project files for C++ course, including main.cpp and project configuration

---
+++ Intro/first_project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Salam!" << endl;
+    cout << "C++";
+
+    return 0;
+}
---
+++ Intro/second_project/program.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Salam!" << endl;
+
+    return 0;
+}
