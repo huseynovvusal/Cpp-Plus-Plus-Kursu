ee3d6a91ba1b5ae6fd75833819c4113b4b454754
Vusal Huseynov <vslh333222111@gmail.com>
2025-07-23 10:10:24 +0400

    Remove outdated introductory documents and add comprehensive content on C++ programming, including execution process, compiler, and interpreter details.

---
+++ 003 Comments/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+// Bu bir serh setridir.
+
+int main()
+{
+    // Bu hissede ekrana yazi cap olunur.
+    cout << "Hello world!" << endl; // Serh
+
+    // Single-line comments
+    // Buraya x ozellik elave edilmelidir.
+    // Yeni bir serh setri.
+
+    /*
+    Multi-line comments
+    Setir 1
+    Setir 2
+    Setir 3
+    */
+
+    /*
+    Multi-line comments
+    Setir 1
+    Setir 2
+    Setir 3
+    */
+
+    return 0;
+}
