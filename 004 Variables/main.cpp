768e28aa57ca057a13d5e4c65cc4ad5d11092de6
Vusal Huseynov <vslh333222111@gmail.com>
2025-07-23 14:15:02 +0400

    Refactor main.cpp to include comprehensive comments and examples for variable declarations and data types in C++

---
+++ 004 Variables/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // x = 5
+    // y = 10
+    // z = x + y
+    // z = 15
+
+    // Declare
+    // Integer - Tam eded
+    // int eded;
+    // eded = 5; // semicolon
+
+    // cout << eded + 5 << endl;
+
+    /*
+    int x = 10;
+    int y = 20;
+    int z = 30;
+    */
+
+    /*
+    int x = 10, y = 20, z = 30;
+
+    int cem = x + y + z;
+
+    cout << x << "+" << y << "+" << z << "=" << cem << endl;
+    */
+
+    // Double
+    // 10.5, 20.3, 0.123
+
+    /*
+    double kesr_eded = 10.5;
+
+    cout << kesr_eded + 1.3 << endl;
+    */
+
+    // Char
+    // Simvol - a, b, c, 1, 2, 3
+
+    /*
+    char simvol = 'a';
+
+    cout << simvol << endl;
+    */
+
+    // String
+    // Setir, Metn, Yazi
+    // string setir = "Salam, C++!";
+    // cout << setir << endl;
+
+    /*
+    string ad = "Ehmed";
+
+    cout << "Salam, " << ad << "!" << endl; */
+
+    // Boolean
+    // true/false
+    /*
+    bool x = 1; // 1
+    bool y = false; // 0
+
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl; */
+
+    // ===================
+
+    int a2 = 10;
+    string first_name = "Vusal", last_name = "Huseynov";
+
+    string ad = "Vusal";
+    string Ad = "Ilkin";
+
+    // string int = "abcd";
+
+    int x != 10;
+
+    return 0;
+}
