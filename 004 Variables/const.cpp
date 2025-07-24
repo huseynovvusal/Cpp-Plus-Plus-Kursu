cbce8ceb6639c4b23f411cf2da6cc62e01c6dc27
Vusal Huseynov <vslh333222111@gmail.com>
2025-07-24 15:33:40 +0400

    Add const.cpp file demonstrating the use of constant variables in C++

---
+++ 004 Variables/const.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const double pi = 3.1415;
+    const double a = 1;
+
+//    pi = 3.15;
+
+    cout << pi << endl;
+
+
+    return 0;
+}
