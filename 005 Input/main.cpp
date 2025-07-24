9408ac22fe2fd42ebbf6b615f1aba0ebba32bd0b
Vusal Huseynov <vslh333222111@gmail.com>
2025-07-24 15:40:04 +0400

    Refactor main.cpp to enhance input/output functionality and remove commented-out code

---
+++ 005 Input/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+// Output
+// Input
+
+int main()
+{
+    //    int x,y;
+
+    //    cout << "Iki eded daxil edin: ";
+    //    cin >> x;
+    //    cin >> y;
+    //    cin >> x >> y;
+
+    //    cout << "Cavab: " << x + y << endl;
+
+    /*     string ad;
+     *
+     *     cout << "Adiniz: ";
+     *     cin >> ad;
+     *
+     *     cout << "Salam, " << ad << endl;
+     */
+
+    double d;
+
+    cin >> d;
+
+    cout << d;
+
+    return 0;
+}
