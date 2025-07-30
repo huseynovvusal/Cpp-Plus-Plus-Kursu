5458b7011d8a3f8d44d44bf9078c6233480a640c
Vusal Huseynov <vslh333222111@gmail.com>
2025-07-30 11:35:54 +0400

    Restore main.cpp with initial implementation and commented-out code for operator demonstrations

---
+++ 006 Operators/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    //    cout << "a,b: ";
+    //    cin >> a >> b;
+
+    //    c = a + b;
+    //    c = a - b;
+    //    c = a * b;
+    //    c = a / b;
+    //    double d = a / b;
+
+    // modulus (mod)
+    //    c = a % b;
+
+    //    c = 10;
+
+    //    c = c + 1;
+
+    //    c++; // c = c + 1;
+
+    //    c = c - 1;
+    //
+    //    c--;
+
+    // Assignment
+
+    // =
+    a = 10;
+    b = 20;
+
+    //    a = a + 10;
+    //    a += 10;
+
+    //    a = a - 10;
+    //    a -= b + 10 * 5;
+
+    //    a /= 3;
+    //    a *= 2;
+
+    a %= 4;
+
+    cout << "Cavab: " << a << endl;
+
+    return 0;
+}
